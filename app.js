4a59654dec4631aecfb6977f92c6f9d7719b5733
IsaiasMarquez28 <14540@virtual.utsc.edu.mx>
2022-01-27 15:41:34 -0600

    Se creo Login y Registro

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
+app.get("/Regiter", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+app.get("/Login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
+});
 
 app.listen(PORT, () => console.log("Servidor" + PORT + " corriendo..."));
